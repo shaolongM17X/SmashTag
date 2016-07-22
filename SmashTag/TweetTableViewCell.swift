893cadc207a6a5757b654144e4c1c14b9cd8fbbd
Shaolong <shaolonglin09@gmail.com>
2016-07-21 23:57:00 -0400

    update #1

--- SmashTag/TweetTableViewCell.swift
+++ SmashTag/TweetTableViewCell.swift
@@ -23,6 +23,36 @@ class TweetTableViewCell: UITableViewCell {
 		}
 	}
 	
+	private struct FontColors {
+		static let HashTagColor = UIColor.redColor()
+		static let URLColor = UIColor.blueColor()
+		static let UserMentionColor = UIColor.cyanColor()
+	}
+	
+	private var fontColors = [
+		"HashTagColor" : UIColor(netHex: 0x0084B4),
+		"URLColor" : UIColor(netHex: 0x0084B4),
+		"UserMentionColor" : UIColor(netHex: 0x0084B4)
+	]
+	
+	private func setColorAttribute(str: NSMutableAttributedString, tag: String, arr: [Mention]) {
+		for item in arr {
+			if let color = fontColors[tag] {
+				str.addAttributes([NSForegroundColorAttributeName: color], range: item.nsrange)
+			}
+			
+		}
+	}
+	
+	private func getAttributedString(tweet: Twitter.Tweet) -> NSMutableAttributedString {
+		let str = NSMutableAttributedString(string: tweet.text)
+		// deal with hashtags
+		setColorAttribute(str, tag: "HashTagColor", arr: tweet.hashtags)
+		setColorAttribute(str, tag: "URLColor", arr: tweet.urls)
+		setColorAttribute(str, tag: "UserMentionColor", arr: tweet.userMentions)
+		return str
+	}
+	
 	private func updateUI() {
 		//reset any existing tweet info
 		tweetTextLabel?.attributedText = nil
@@ -32,12 +62,15 @@ class TweetTableViewCell: UITableViewCell {
 		
 		// load new information from tweet
 		if let tweet = self.tweet {
-			tweetTextLabel?.text = tweet.text
+			
+			// tweet content
+			tweetTextLabel?.attributedText = getAttributedString(tweet)
 			if tweetTextLabel?.text != nil {
 				for _ in tweet.media {
 					tweetTextLabel.text! += " 📸"
 				}
 			}
+			
 			
 			tweetScreenNameLabel?.text = "\(tweet.user)"
 			
@@ -68,4 +101,20 @@ class TweetTableViewCell: UITableViewCell {
 //        // Configure the view for the selected state
 //    }
 
+	
+}
+
+// supporting method used to generate color using hex
+extension UIColor {
+	convenience init(red: Int, green: Int, blue: Int) {
+		assert(red >= 0 && red <= 255, "Invalid red component")
+		assert(green >= 0 && green <= 255, "Invalid green component")
+		assert(blue >= 0 && blue <= 255, "Invalid blue component")
+		
+		self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+	}
+	
+	convenience init(netHex:Int) {
+		self.init(red:(netHex >> 16) & 0xff, green:(netHex >> 8) & 0xff, blue:netHex & 0xff)
+	}
 }
